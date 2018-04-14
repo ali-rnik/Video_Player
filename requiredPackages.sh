2fd1ac5bb706d10ae67f4801eaa00f76470ff0e7
Ahmadreza Hosseini Modaghegh <aiispassw@gmail.com>
2018-04-14 18:19:14 +0430

    Update make-environment-ready files

--- requiredPackages.sh
+++ requiredPackages.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-#installing OpenCV requirements , links : https://docs.opencv.org/master/d7/d9f/tutorial_linux_install.html
-sudo apt-get install build-essential cmake git libgtk2.0-dev libgtk-3-0 libgtk-3-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev git
+# check if root access provided by user
+if [ "$EUID" -ne 0 ]; then
+    echo "This bash file will install some required packages on your device. Please provide root access by executing \"sudo ./requiredPackages.sh\"."
+    exit
+fi
 
+echo "Do you want to install some optional packages too? (yes/no) "
+read install_optional_packages
+
+# determine required packages to be installed
+packages=""
+if (( $install_optional_packages == "yes" )); then
+    $packages="cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev"
+else
+    $packages="cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev"
+fi
+
+# If you are interested in openCV installation process you may want to visit: https://docs.opencv.org/master/d7/d9f/tutorial_linux_install.html
 #clone OpenCV from git
 git clone https://github.com/opencv/opencv.git
 cd opencv
 mkdir build
 cd build
-
-#compiling git
 cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_PREFIX=/usr/local ..
-make -j $(nproc)
+make -j7 # runs 7 jobs in parallel
 sudo make install
 
